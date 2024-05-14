2c7ce810acb94e4a098742e64ec7971cc270aa73
helvis <helvis.eloy1998@gmail.com>
2024-05-14 18:26:26 -0400

    tomando a partir del repositorio original

--- colors.py
+++ colors.py
@@ -1,19 +1,18 @@
 class Colors:
-    dark_grey=(26,31,40)
-    green=(47,230,23)
-    red=(232,18,18)
-    orange=(226,116,17)
-    yellow=(237,234,4)
-    purple=(166,0,247)
-    cyan=(21,204,209)
-    blue=(13,64,216)
-    white=(255,255,255)
-    dark_blue=(44,44,127) #color de fondo
-    light_blue=(59,85,162)
-    
-    #es un pyhon decorator que permite definir un metodo que puedeser llamado
+	dark_grey = (26, 31, 40)
+	green = (47, 230, 23)
+	red = (232, 18, 18)
+	orange = (226, 116, 17)
+	yellow = (237, 234, 4)
+	purple = (166, 0, 247)
+	cyan = (21, 204, 209)
+	blue = (13, 64, 216)
+	white = (255, 255, 255)
+	dark_blue = (44, 44, 127)
+	light_blue = (59, 85, 162)
+
+	#es un pyhon decorator que permite definir un metodo que puedeser llamado
     #en una clase en lugar de una instancia
-    
-    @classmethod
-    def get_cell_colors(cls):
-        return[cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
+	@classmethod
+	def get_cell_colors(cls):
+		return [cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
--- grid.py
+++ grid.py
@@ -2,70 +2,69 @@ import pygame
 from colors import Colors
 
 class Grid:
-    def __init__(self):
-        self.num_rows=20
-        self.num_cols=10
-        self.cell_size=30 #tamaño de la celda
+	def __init__(self):
+		self.num_rows = 20
+		self.num_cols = 10
+		self.cell_size = 30 #tamaño de la celda
         #una lista de listas
                     #lista de 10 ceros       y luego repite hasta completar 20 filas
-        self.grid=[[0 for j in range (self.num_cols)] for i in range (self.num_rows)]
-        self.colors=Colors.get_cell_colors()
-        
-    def print_grid(self):
-        for row in range(self.num_rows):
-            for column in range(self.num_cols):
-                print(self.grid[row][column], end =" ")
-            print()
-            
-    #evitar que se salgan
-    def is_inside(self, row, column):
-        if row >= 0 and row < self.num_rows and column >= 0 and column<self.num_cols:
-            return True
-        return False
- #METODOS PARA CUANDO SE COMPLETAN FILAS       
-    def is_empty(self,row,column):
-        if self.grid[row][column]==0:
-            return True
-        return False
-    
-    def is_row_full(self, row):
-        for column in range(self.num_cols):
-            if self.grid[row][column] == 0:
-                return False
-        return True
+		self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+		self.colors = Colors.get_cell_colors()
 
-    def clear_row(self, row):
-        for column in range(self.num_cols):
-            self.grid[row][column] = 0
-    
-    def move_row_down(self, row, num_rows):
-        for column in range(self.num_cols):
-            self.grid[row+num_rows][column] = self.grid[row][column]
-            self.grid[row][column] = 0
-            
-    def clear_full_rows(self):
-        completed = 0
-        for row in range(self.num_rows-1, 0, -1):
-            if self.is_row_full(row):
-                self.clear_row(row)
-                completed += 1
-            elif completed > 0:
-                self.move_row_down(row, completed)
-        return completed
-    
-    def reset(self):
-        for row in range(self.num_rows):
-            for column in range(self.num_cols):
-                self.grid[row][column] = 0    
-    
-#*****************************************************************************
-    def draw (self, screen): #recorre el grid y asigna el valor a las celdas, todas empiezan en 0
-        for row in range(self.num_rows):
-            for column in range(self.num_cols):
-                #asignamos la columna 00 01 21 53 en cada iteracion
-                cell_value=self.grid[row][column]
-                                     #     x                    y                    w                     h
-                cell_rect=pygame.Rect(column*self.cell_size+11, row*self.cell_size+11, self.cell_size-1, self.cell_size-1)
-            #agregamos esos 1 para separar los cuadrados y dejar ver lineas del color del fondo, en este caso el dark blue
+	def print_grid(self):
+		for row in range(self.num_rows):
+			for column in range(self.num_cols):
+				print(self.grid[row][column], end = " ")
+			print()
+	
+ 	#evitar que se salgan
+	def is_inside(self, row, column):
+		if row >= 0 and row < self.num_rows and column >= 0 and column < self.num_cols:
+			return True
+		return False
+
+#METODOS PARA CUANDO SE COMPLETAN FILAS-----------------------------------------------------------------
+	def is_empty(self, row, column):
+		if self.grid[row][column] == 0:
+			return True
+		return False
+
+	def is_row_full(self, row):
+		for column in range(self.num_cols):
+			if self.grid[row][column] == 0:
+				return False
+		return True
+
+	def clear_row(self, row):
+		for column in range(self.num_cols):
+			self.grid[row][column] = 0
+
+	def move_row_down(self, row, num_rows):
+		for column in range(self.num_cols):
+			self.grid[row+num_rows][column] = self.grid[row][column]
+			self.grid[row][column] = 0
+
+	def clear_full_rows(self):
+		completed = 0
+		for row in range(self.num_rows-1, 0, -1):
+			if self.is_row_full(row):
+				self.clear_row(row)
+				completed += 1
+			elif completed > 0:
+				self.move_row_down(row, completed)
+		return completed
+
+	def reset(self):
+		for row in range(self.num_rows):
+			for column in range(self.num_cols):
+				self.grid[row][column] = 0
+#--------------------------------------------------------------------------------------------------
+	def draw(self, screen):#recorre el grid y asigna el valor a las celdas, todas empiezan en 0
+		for row in range(self.num_rows):
+			for column in range(self.num_cols):
+       		#asignamos la columna 00 01 21 53 en cada iteracion
+				cell_value = self.grid[row][column]
+				cell_rect = pygame.Rect(column*self.cell_size + 11, row*self.cell_size + 11,self.cell_size -1, self.cell_size -1)
+    #agregamos esos 11 para separar los cuadrados y dejar ver lineas del color del fondo, en este caso el dark blue
                 # recibe la variable pantalla que creamos en main
-                pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
+				pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
